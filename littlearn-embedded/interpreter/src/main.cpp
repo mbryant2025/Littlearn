3f7ad752e9647ea32b65e56d064f0bf32c0ba844
Michael Bryant <mcb115@duke.edu>
2023-10-08 01:32:52 -0400

    demo program for current progress

--- littlearn-embedded/interpreter/src/main.cpp
+++ littlearn-embedded/interpreter/src/main.cpp
@@ -7,7 +7,7 @@ int main()
 {
     // Input source code
     // std::string sourceCode = "int sum = 0; float pi = 3.14; //this is a comment\n while (sum < 5) {sum = sum + 1;};";
-    std::string sourceCode = "{int sum = 5; float x = 4.43; print(x + sum);}";
+    std::string sourceCode = "{int sum = 5; float x = 4.43; print(x + sum); print(x + x); float w = 3.14 + x; print(w + sum);}";
 
     // Create a Tokenizer object
     Tokenizer tokenizer(sourceCode);
@@ -20,11 +20,11 @@ int main()
 
     BlockNode* block = parser.parseProgram();
 
-    std::cout << "AST: " << std::endl;
+    // std::cout << "AST: " << std::endl;
 
-    std::cout << block->toString() << std::endl;
+    // std::cout << block->toString() << std::endl;
 
-    std::cout << "==========================================================" << std::endl;
+    // std::cout << "==========================================================" << std::endl;
 
     // Create an Interpreter object
     Interpreter interpreter(block);
