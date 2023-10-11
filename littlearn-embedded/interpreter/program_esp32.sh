82d84b4aab30154278f29d8274bc18ec34069f34
Michael Bryant <mcb115@duke.edu>
2023-10-10 23:01:28 -0400

    temp move hpp

--- littlearn-embedded/interpreter/program_esp32.sh
+++ littlearn-embedded/interpreter/program_esp32.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Directory containing this script
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 # Install the ESP32 Core for Arduino
 arduino-cli core install esp32:esp32
 
@@ -13,8 +10,10 @@ arduino-cli lib install BLEPeripheral
 arduino-cli core update-index
 arduino-cli core upgrade esp32:esp32
 
+
 # Find the port of the connected ESP32
 port=$(arduino-cli board list | grep "ttyUSB" | cut -d " " -f 1)
+# port="/dev/ttyUSB0"
 
 # Check if a port was found
 if [ -z "$port" ]; then
@@ -26,14 +25,23 @@ fi
 
 echo -e "\e[32mCompiling ESP32 sketch...\e[0m"
 
-# Compile the ESP32 sketch, including the "include" directory
-arduino-cli compile --fqbn esp32:esp32:esp32 --build-path "$script_dir/build" "$script_dir/esp32"
+# Temporarily copy the .hpp files to the sketch folder
+cp ../interpreter/*.hpp esp32/
+
+# Compile the ESP32 sketch
+arduino-cli compile --fqbn esp32:esp32:esp32 esp32 --clean
 
 # Check the compilation status
 if [ $? -eq 0 ]; then
-    echo -e "\e[32mCompilation successful. Uploading to ESP32...\e[0m"
-    arduino-cli upload -b esp32:esp32:esp32 --port "$port" --input "$script_dir/build/esp32.ino.bin"
+
+    echo -e "\e[32mCompilation finished. Uploading to ESP32...\e[0m"
+    
+    # compile with an additional folder of .hpp files
+    arduino-cli upload -b esp32:esp32:esp32 esp32 -p $port
 else
     echo -e "\e[31mCompilation failed.\e[0m"
     exit 1
 fi
+
+# Remove the temporary .hpp files
+rm esp32/*.hpp
