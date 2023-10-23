c8762b1af43926c5acba9942948ddad8e2854822
wrd10 <wrd10@duke.edu>
2023-10-23 18:19:20 -0400

    fixed in line op

--- littlearn-web/src/blockly/blocks/binaryOp.js
+++ littlearn-web/src/blockly/blocks/binaryOp.js
@@ -6,6 +6,11 @@ const binaryOp = {
   'message0': '%1 %2 %3',
   'args0': [
     {
+      'type': 'input_value',
+      'name': 'A',
+      'check': 'Number',
+    },
+    {
       'type': 'field_dropdown',
       'name': 'OPERATOR',
       'options': [
@@ -21,15 +26,12 @@ const binaryOp = {
     },
     {
       'type': 'input_value',
-      'name': 'A',
-      'check': 'Number',
-    },
-    {
-      'type': 'input_value',
       'name': 'B',
       'check': 'Number',
     },
   ],
+  //make in line
+  'inputsInline': true,
   'output': 'Number',
   'colour': 160,
   'tooltip': '',
