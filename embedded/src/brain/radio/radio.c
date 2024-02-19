4639967fa6b23d56ae48c75c5e718fdffaceec68
mbryant2025 <mbryant2025@gmail.com>
2024-02-19 17:30:31 -0500

    concurrent espnow and ble

--- embedded/src/brain/radio/radio.c
+++ embedded/src/brain/radio/radio.c
@@ -1,12 +1,3 @@
-/* Get Start Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
@@ -14,76 +5,14 @@
 #include "esp_system.h"
 #include "esp_wifi.h"
 
-#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 4, 0)
 #include "esp_mac.h"
-#endif
 
 #include "espnow.h"
 #include "espnow_storage.h"
 #include "espnow_utils.h"
 
-// #include "driver/uart.h"
-
-// You can modify these according to your boards.
-// #define UART_BAUD_RATE 115200
-// #define UART_PORT_NUM  0
-// #define UART_TX_IO     UART_PIN_NO_CHANGE
-// #define UART_RX_IO     UART_PIN_NO_CHANGE
-
 static const char *TAG = "app_main";
 
-// static void app_uart_read_task(void *arg)
-// {
-//     esp_err_t ret  = ESP_OK;
-//     uint32_t count = 0;
-//     size_t size    = 0;
-//     uint8_t *data  = ESP_CALLOC(1, ESPNOW_DATA_LEN);
-
-//     ESP_LOGI(TAG, "Uart read handle task is running");
-
-//     espnow_frame_head_t frame_head = {
-//         .retransmit_count = CONFIG_RETRY_NUM,
-//         .broadcast        = true,
-//     };
-
-//     for (;;) {
-//         size = uart_read_bytes(UART_PORT_NUM, data, ESPNOW_DATA_LEN, pdMS_TO_TICKS(10));
-//         ESP_ERROR_CONTINUE(size <= 0, "");
-
-//         ret = espnow_send(ESPNOW_DATA_TYPE_DATA, ESPNOW_ADDR_BROADCAST, data, size, &frame_head, portMAX_DELAY);
-//         ESP_ERROR_CONTINUE(ret != ESP_OK, "<%s> espnow_send", esp_err_to_name(ret));
-
-//         ESP_LOGI(TAG, "espnow_send, count: %" PRIu32 ", size: %u, data: %s", count++, size, data);
-//         memset(data, 0, ESPNOW_DATA_LEN);
-//     }
-
-//     ESP_LOGI(TAG, "Uart handle task is exit");
-
-//     ESP_FREE(data);
-//     vTaskDelete(NULL);
-// }
-
-// static void app_uart_initialize()
-// {
-//     uart_config_t uart_config = {
-//         .baud_rate = UART_BAUD_RATE,
-//         .data_bits = UART_DATA_8_BITS,
-//         .parity    = UART_PARITY_DISABLE,
-//         .stop_bits = UART_STOP_BITS_1,
-//         .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-// #if SOC_UART_SUPPORT_REF_TICK
-//         .source_clk = UART_SCLK_REF_TICK,
-// #elif SOC_UART_SUPPORT_XTAL_CLK
-//         .source_clk = UART_SCLK_XTAL,
-// #endif
-//     };
-
-//     ESP_ERROR_CHECK(uart_param_config(UART_PORT_NUM, &uart_config));
-//     ESP_ERROR_CHECK(uart_set_pin(UART_PORT_NUM, UART_TX_IO, UART_RX_IO, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
-//     ESP_ERROR_CHECK(uart_driver_install(UART_PORT_NUM, 8 * ESPNOW_DATA_LEN, 8 * ESPNOW_DATA_LEN, 0, NULL, 0));
-
-//     xTaskCreate(app_uart_read_task, "app_uart_read_task", 4 * 1024, NULL, tskIDLE_PRIORITY + 1, NULL);
-// }
 
 static void app_wifi_init()
 {
@@ -92,13 +21,13 @@ static void app_wifi_init()
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
 
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));
     ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
     ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
     ESP_ERROR_CHECK(esp_wifi_start());
 }
 
-static esp_err_t app_uart_write_handle(uint8_t *src_addr, void *data,
+static esp_err_t write_handle(uint8_t *src_addr, void *data,
                                        size_t size, wifi_pkt_rx_ctrl_t *rx_ctrl)
 
 {
@@ -126,12 +55,11 @@ void radio_init()
 {
     espnow_storage_init();
 
-    // app_uart_initialize();
     app_wifi_init();
 
     espnow_config_t espnow_config = ESPNOW_INIT_CONFIG_DEFAULT();
     espnow_init(&espnow_config);
 
-    espnow_set_config_for_data_type(ESPNOW_DATA_TYPE_DATA, true, app_uart_write_handle);
+    espnow_set_config_for_data_type(ESPNOW_DATA_TYPE_DATA, true, write_handle);
 
 }
